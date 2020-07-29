0dbeaf8f94a714e083cfbed8260081b76502c5a7
dhanarajknp <kdhanaraj@gmail.com>
2020-07-29 23:39:44 +0530

    http://localhost:8091/swagger-ui.html

--- spring-security-jwt-master/src/main/java/com/studycool/config/WebSecurityConfig.java
+++ spring-security-jwt-master/src/main/java/com/studycool/config/WebSecurityConfig.java
@@ -17,7 +17,20 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import javax.annotation.Resource;
 
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+
+
 @Configuration
+@EnableSwagger2
 @EnableWebSecurity
 //@EnableGlobalMethodSecurity(securedEnabled = true)
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -50,16 +63,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().
                 authorizeRequests()
-                .antMatchers("/token/*", "/signup").permitAll()
-                .antMatchers(HttpMethod.GET,"/api/univercity","/api/role/regs","/api/course/univercity/{id}","/api/forgetpassword/{username}").permitAll()
+                .antMatchers("/token/*", "/signup","/h2-console/**").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/univercity","/api/role/regs","/api/course/univercity/{id}","/api/forgetpassword/{username}","/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http
-                .addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
+                 http.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
     }
 
+    
+   @Bean
+   public Docket api() { 
+       return new Docket(DocumentationType.SWAGGER_2)  
+         .select()                                  
+         .apis(RequestHandlerSelectors.any())              
+         .paths(PathSelectors.any())     
+         .build();                                            
+   }
     @Bean
     public BCryptPasswordEncoder encoder(){
         return new BCryptPasswordEncoder();
